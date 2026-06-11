087bec36dbbee78d800bc6bd8344ee81675871a8
xiaolongbao <daixuefeng@5i5j.com>
2026-06-11 09:16:12 +0800

    LPL 2026-06-11

--- auto_lpl.sh
+++ auto_lpl.sh
@@ -11,10 +11,10 @@ current_date=$(date +%Y-%m-%d)
 python3 LPL_T.py
 python3 convert_to_ics.py
 
-if git diff --cached --quiet; then
-  echo "没有检测到变更，跳过提交和推送。"
-  exit 0
-fi
+# if git diff --cached --quiet; then
+#   echo "没有检测到变更，跳过提交和推送。"
+#   exit 0
+# fi
 
 git add .
 
